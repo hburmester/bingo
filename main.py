be6ce38e20974c73b3326365395bbbb074c22ca2
Hans Burmester Wong <52086563+hburmester@users.noreply.github.com>
2023-09-07 17:54:34 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+# for i in range(5):
+#                 fila = []
+#                 for j in range(15):
+#                     a = "--"
+#                     while a in fila:
+#                         a = "--"
+#                     fila.append(a)
+#                 matriz.append(fila)
+#             print(matriz)
+#             for i in range(1):
+#                 matriz[i][i] = randint((i + 1))
+#                 archivo = open ("Guardando numeros que salen.txt", "a")
+#
+#
+#     matriz = np.array(matriz)
+#     matriz = np.transpose(matriz)
+#     matriz = str(matriz)
+#     matriz = matriz.replace("[", "")
+#     matriz = matriz.replace("]", "")
+#     matriz = matriz.replace("'", "")
+#     matriz = matriz.replace("\n ", "\n")
+#     print(matriz)
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    for i in range(75):
+        if i%15 == 0:
+            print(i/15)
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
